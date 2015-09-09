e9a6888c79f347b2b6b16ec89630336e370eeba6
Nick <nickbooties@users.noreply.github.com>
2015-09-09 12:09:22 +1000

    fixed date parsing

--- controller/accessstatscontroller.php
+++ controller/accessstatscontroller.php
@@ -17,35 +17,37 @@ class AccessStatsController extends Controller {
 
    
     public function getstats() {
-	
-	$timeperiod = json_decode($_POST['accessstats']);
-	
-	$from_timestamp = strtotime($timeperiod->from_date);
-	$to_timestamp= strtotime($timeperiod->to_date + 86400);
-	
-	$sql = 'SELECT id, time, username, path FROM `*PREFIX*accessstats_items` WHERE time >= ? and time <= ?';
-	
-	$query = \OCP\DB::prepare($sql);
-	$result = $query->execute(array($from_timestamp, $to_timestamp));
-	
-	$data =  "id,time,username,path\r\n";
-	
-	while($row = $result->fetchRow()) {
-	    $data .= "\"".$row['id']."\",\"".date("H:i:s d/m/Y",$row['time'])."\",\"".$row['username']."\",\"".$row['path']."\"\r\n";
-	}
-	
-	header('Content-Description: File Transfer');
 
-	//header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename=access_report.csv');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate');
-	header('Pragma: public');
-	header('Content-Length: ' . strlen($data));
-	
-	print($data);
-	exit(0);
-	
+        $timeperiod = json_decode($_POST['accessstats']);
+
+        $from_timestamp = date("Y-m-d 0:0:0",strtotime($timeperiod->from_date));
+        $to_timestamp= date("Y-m-d 23:59:59",strtotime($timeperiod->to_date));
+
+        $sql = 'SELECT id, time, username, path FROM `*PREFIX*accessstats_items` WHERE time >= ? and time <= ?';
+
+        error_log($from_timestamp);
+        $query = \OCP\DB::prepare($sql);
+        $query->bindparam(1,$from_timestamp);
+        $query->bindparam(2,$to_timestamp);
+        $result = $query->execute();
+        $data =  "id,time,username,path\r\n";
+
+        while($row = $result->fetchRow()) {
+            $data .= "\"".$row['id']."\",\"".date("H:i:s d/m/Y",$row['time'])."\",\"".$row['username']."\",\"".$row['path']."\"\r\n";
+        }
+
+        header('Content-Description: File Transfer');
+
+        //header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=access_report.csv');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . strlen($data));
+
+        print($data);
+        exit(0);
+
     }
 
 }
